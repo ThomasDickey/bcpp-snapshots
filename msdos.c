b7e4d2ab54e2702b69ec4aa02e5825a4a8922549
Thomas E. Dickey <dickey@invisible-island.net>
2004-06-19 14:38:53 -0400

    snapshot of project "conflict", label t20040619

--- msdos.c
+++ msdos.c
@@ -1,12 +1,25 @@
 /******************************************************************************
- * Copyright 1995 by Thomas E. Dickey.  All Rights Reserved.                  *
+ * Copyright 1995,2004 by Thomas E. Dickey.  All Rights Reserved.             *
  *                                                                            *
- * You may freely copy or redistribute this software, so long as there is no  *
- * profit made from its use, sale trade or reproduction. You may not change   *
- * this copyright notice, and it must be included in any copy made.           *
+ * Permission to use, copy, modify, and distribute this software and its      *
+ * documentation for any purpose and without fee is hereby granted, provided  *
+ * that the above copyright notice appear in all copies and that both that    *
+ * copyright notice and this permission notice appear in supporting           *
+ * documentation, and that the name of the above listed copyright holder(s)   *
+ * not be used in advertising or publicity pertaining to distribution of the  *
+ * software without specific, written prior permission.                       *
+ *                                                                            *
+ * THE ABOVE LISTED COPYRIGHT HOLDER(S) DISCLAIM ALL WARRANTIES WITH REGARD   *
+ * TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND  *
+ * FITNESS, IN NO EVENT SHALL THE ABOVE LISTED COPYRIGHT HOLDER(S) BE LIABLE  *
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES          *
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN      *
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR *
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.                *
  ******************************************************************************/
+
 #ifndef NO_IDENT
-static char *Id = "$Id: msdos.c,v 6.3 2004/03/27 20:39:22 tom Exp $";
+static const char Id[] = "$Id: msdos.c,v 6.5 2004/06/19 14:37:53 tom Exp $";
 #endif
 
 /*
@@ -17,152 +30,154 @@ static char *Id = "$Id: msdos.c,v 6.3 2004/03/27 20:39:22 tom Exp $";
 #include <ctype.h>
 
 char *
-fleaf (char *name)
+fleaf(char *name)
 {
-	char	*s;
+    char *s;
 
-	while ((s = strpbrk(name, ":/\\")) != 0) {
-		name = s+1;
-	}
-	return name;
+    while ((s = strpbrk(name, ":/\\")) != 0) {
+	name = s + 1;
+    }
+    return name;
 }
 
 char *
-ftype (char *name)
+ftype(char *name)
 {
-	char	*leaf = fleaf(name);
-	char	*type = strrchr(leaf, '.');
-	if (type == 0)
-		type = leaf + strlen(leaf);
-	return type;
+    char *leaf = fleaf(name);
+    char *type = strrchr(leaf, '.');
+    if (type == 0)
+	type = leaf + strlen(leaf);
+    return type;
 }
 
-void	blip(int c)
+void
+blip(int c)
 {
-	putc(c, stderr);
-}
-
-int
-have_drive (char *name)
-{
-	return (isalpha(name[0]) && name[1] == ':');
+    putc(c, stderr);
 }
 
 int
-same_drive (char *a, char *b)
+have_drive(char *name)
 {
-	return have_drive(a) && have_drive(b) && (*a == *b);
+    return (isalpha(name[0]) && name[1] == ':');
 }
 
 int
-set_directory (char *name)
+same_drive(char *a, char *b)
 {
-	return set_drive(name) && (chdir(name) >= 0);
+    return have_drive(a) && have_drive(b) && (*a == *b);
+}
+
+int
+set_directory(char *name)
+{
+    return set_drive(name) && (chdir(name) >= 0);
 }
 
 #if SYS_MSDOS
 #include <dos.h>		/* ...for _getdrive/_setdrive */
 
 int
-set_drive (char *name)
+set_drive(char *name)
 {
-	if (have_drive(name)) {
-		unsigned want = name[0] - 'A';
-		unsigned have;
-		bdos(0x0e, want, 0); 			/* set drive */
-		have = (bdos(0x19, 0, 0) & 0xff);	/* get drive */
-		if (want != have)
-			return FALSE;
-	}
-	return TRUE;
+    if (have_drive(name)) {
+	unsigned want = name[0] - 'A';
+	unsigned have;
+	bdos(0x0e, want, 0);	/* set drive */
+	have = (bdos(0x19, 0, 0) & 0xff);	/* get drive */
+	if (want != have)
+	    return FALSE;
+    }
+    return TRUE;
 }
-#endif	/* SYS_MSDOS */
+#endif /* SYS_MSDOS */
 
 #if SYS_OS2 || SYS_OS2_EMX
 #include <dos.h>		/* ...for _getdrive/_setdrive */
 
 int
-set_drive (char *name)
+set_drive(char *name)
 {
-	if (have_drive(name)) {
-		if (_chdrive(name[0] + 1 - 'A') < 0)
-			return FALSE;
-	}
-	return TRUE;
+    if (have_drive(name)) {
+	if (_chdrive(name[0] + 1 - 'A') < 0)
+	    return FALSE;
+    }
+    return TRUE;
 }
-#endif	/* SYS_OS2 || SYS_OS2_EMX */
+#endif /* SYS_OS2 || SYS_OS2_EMX */
 
 #if SYS_WIN32
 #include <dos.h>		/* ...for _getdrive/_setdrive */
 
 int
-set_drive (char *name)
+set_drive(char *name)
 {
-	if (have_drive(name)) {
-		if (_chdrive(name[0] + 1 - 'A') < 0)
-			return FALSE;
-	}
-	return TRUE;
+    if (have_drive(name)) {
+	if (_chdrive(name[0] + 1 - 'A') < 0)
+	    return FALSE;
+    }
+    return TRUE;
 }
-#endif	/* SYS_WIN32 */
-
+#endif /* SYS_WIN32 */
 
 #ifndef HAVE_GETOPT
-int	optind;
-char	*optarg;
+int optind;
+char *optarg;
 
-	/*
-	 * This version of 'getopt()' uses either '/' or '-' for the switch
-	 * character to look more like a native dos or os/2 application.
-	 */
-int	getopt(int argc, char *const *argv, const char *opts)
+/*
+ * This version of 'getopt()' uses either '/' or '-' for the switch
+ * character to look more like a native dos or os/2 application.
+ */
+int
+getopt(int argc, char *const *argv, const char *opts)
 {
-	int	code = EOF;
-	char	*s, *t;
+    int code = EOF;
+    char *s, *t;
 
-	if (optind == 0) {
-		optind = 1;
-		optarg = "";
-	}
-	if (optind < argc) {
-		s = argv[optind];
-		if (*s == '/' || *s == '-') {
-			optind++;
-			code = '?';
-			if (*++s != EOS) {
-				t = strchr(opts, *s++);
-				if (t != 0 && *t != ':') {
-					code = *t++;
-					if (*t == ':') {
-						if (*s != EOS)
-							optarg = s;
-						else if (optind < argc)
-							optarg = argv[optind++];
-					} else if (*s != EOS) {
-						optind--;
-						while ((s[-1] = s[0]) != EOS)
-							s++;
-					}
-				}
-			}
+    if (optind == 0) {
+	optind = 1;
+	optarg = "";
+    }
+    if (optind < argc) {
+	s = argv[optind];
+	if (*s == '/' || *s == '-') {
+	    optind++;
+	    code = '?';
+	    if (*++s != EOS) {
+		t = strchr(opts, *s++);
+		if (t != 0 && *t != ':') {
+		    code = *t++;
+		    if (*t == ':') {
+			if (*s != EOS)
+			    optarg = s;
+			else if (optind < argc)
+			    optarg = argv[optind++];
+		    } else if (*s != EOS) {
+			optind--;
+			while ((s[-1] = s[0]) != EOS)
+			    s++;
+		    }
 		}
+	    }
 	}
-	return code;
+    }
+    return code;
 }
 #endif
 
-#if	!HAVE_REALPATH
+#if !HAVE_REALPATH
 /*
  * Note: OS/2 EMX fixes slashes in _fullpath().
  */
-char	*my_realpath(char *given, char *actual)
+char *
+my_realpath(char *given, char *actual)
 {
-	char *result;
-	if (_fullpath(actual, given, MAXPATHLEN) == 0) {
-		result = actual;
-	} else {
-		result = given;
-	}
-	return result;
+    char *result;
+    if (_fullpath(actual, given, MAXPATHLEN) == 0) {
+	result = actual;
+    } else {
+	result = given;
+    }
+    return result;
 }
 #endif
--- unix.c
+++ unix.c
@@ -1,97 +1,108 @@
 /******************************************************************************
- * Copyright 1995 by Thomas E. Dickey.  All Rights Reserved.                  *
+ * Copyright 1995,2004 by Thomas E. Dickey.  All Rights Reserved.             *
  *                                                                            *
- * You may freely copy or redistribute this software, so long as there is no  *
- * profit made from its use, sale trade or reproduction. You may not change   *
- * this copyright notice, and it must be included in any copy made.           *
+ * Permission to use, copy, modify, and distribute this software and its      *
+ * documentation for any purpose and without fee is hereby granted, provided  *
+ * that the above copyright notice appear in all copies and that both that    *
+ * copyright notice and this permission notice appear in supporting           *
+ * documentation, and that the name of the above listed copyright holder(s)   *
+ * not be used in advertising or publicity pertaining to distribution of the  *
+ * software without specific, written prior permission.                       *
+ *                                                                            *
+ * THE ABOVE LISTED COPYRIGHT HOLDER(S) DISCLAIM ALL WARRANTIES WITH REGARD   *
+ * TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND  *
+ * FITNESS, IN NO EVENT SHALL THE ABOVE LISTED COPYRIGHT HOLDER(S) BE LIABLE  *
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES          *
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN      *
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR *
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.                *
  ******************************************************************************/
+
 #ifndef NO_IDENT
-static char *Id = "$Id: unix.c,v 6.0 1995/03/15 00:12:22 dickey Rel $";
+static const char Id[] = "$Id: unix.c,v 6.2 2004/06/19 14:38:08 tom Exp $";
 #endif
 
 #include "conflict.h"
 
-void	blip(c)
-	int	c;
+void
+blip(int c)
 {
-	static	char	text[] = "?";
-	text[0] = (char)c;
-	write(fileno(stderr), text, 1);
-}
-
-char *	fleaf(name)
-	char	*name;
-{
-	char	*leaf = strrchr(name, PATHNAME_SEP);
-	if (leaf != 0)
-		leaf++;
-	else
-		leaf = name;
-	return leaf;
+    static char text[] = "?";
+    text[0] = (char) c;
+    write(fileno(stderr), text, 1);
 }
 
 char *
-ftype (name)
-	char	*name;
+fleaf(char *name)
 {
-	char	*leaf = fleaf(name);
-	char	*type = strrchr(leaf, '.');
-	if (type == 0)
-		type = leaf + strlen(leaf);
-	return type;
+    char *leaf = strrchr(name, PATHNAME_SEP);
+    if (leaf != 0)
+	leaf++;
+    else
+	leaf = name;
+    return leaf;
+}
+
+char *
+ftype(char *name)
+{
+    char *leaf = fleaf(name);
+    char *type = strrchr(leaf, '.');
+    if (type == 0)
+	type = leaf + strlen(leaf);
+    return type;
 }
 
 #if !USE_INODE && !HAVE_REALPATH
-char	*my_realpath(given, actual)
-	char	*given;
-	char	*actual;
+char *
+my_realpath(char *given, char *actual)
 {
-	char	current[MAXPATHLEN];
-	char	*extra = 0;
-	char	*result = 0;
+    char current[MAXPATHLEN];
+    char *extra = 0;
+    char *result = 0;
 
-	if (getwd(current) != 0) {
-		struct	stat	sb;
+    if (getwd(current) != 0) {
+	struct stat sb;
 
-		if (stat(strcpy(actual, given), &sb) >= 0) {
-			if ((sb.st_mode & S_IFMT) != S_IFDIR) {
-				extra = fleaf(given);
-				if (extra == given) {
-					(void) strcpy(actual, current);
-				} else {
-					actual[(int)(extra-given)] = EOS;
-				} 	
-			}
-			if (!strcmp(actual, current)) {
-				result = actual;
-			} else if (chdir(actual) >= 0) {
-				if (getwd(actual) != 0) {
-					result = actual;
-				}
-				(void)chdir(current);
-			}
-			if (result != 0 && extra != 0) {
-				char *s = actual + strlen(actual);
-				*s++ = PATHNAME_SEP;
-				(void)strcpy(s, extra);
-			}
+	if (stat(strcpy(actual, given), &sb) >= 0) {
+	    if ((sb.st_mode & S_IFMT) != S_IFDIR) {
+		extra = fleaf(given);
+		if (extra == given) {
+		    (void) strcpy(actual, current);
+		} else {
+		    actual[(int) (extra - given)] = EOS;
 		}
+	    }
+	    if (!strcmp(actual, current)) {
+		result = actual;
+	    } else if (chdir(actual) >= 0) {
+		if (getwd(actual) != 0) {
+		    result = actual;
+		}
+		(void) chdir(current);
+	    }
+	    if (result != 0 && extra != 0) {
+		char *s = actual + strlen(actual);
+		*s++ = PATHNAME_SEP;
+		(void) strcpy(s, extra);
+	    }
 	}
-	return result;
+    }
+    return result;
 }
 #endif
 
 #if !HAVE_STRDUP
-char	*my_strdup(s)
-	char	*s;
+char *
+my_strdup(char *s)
 {
-	if (s != 0) {
-		char	*t = malloc(strlen(s)+1);
-		if (t != 0) {
-			(void)strcpy(t, s);
-		}
-		s = t;
+    if (s != 0) {
+	char *t = malloc(strlen(s) + 1);
+	if (t != 0) {
+	    (void) strcpy(t, s);
 	}
-	return s;
+	s = t;
+    }
+    return s;
 }
 #endif
